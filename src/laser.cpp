219dc49a54b66531664ac4d630129f4e6fd887f3
Alex Fernandez <javier.fddz@gmail.com>
2022-07-07 11:15:01 +0200

    Update laser lib and Add rcv pin

--- src/laser.cpp
+++ src/laser.cpp
@@ -2,16 +2,22 @@
 
 #include <laser.h>
 
-#define TX_SPEED 4 // bits/s
-#define LO 10  // 0-bit
-#define HI 255 // 1-bit
+// CONFIGS (*)
+#define RECEIVER_ON_LVL 20 // 0 - 1023
 
-int shoot_delay = 1000 / TX_SPEED;
+#define TX_SPEED 4 // bits/s (*)
+#define LO 10  // 0-bit (*)
+#define HI 255 // 1-bit (*)
 
-void setupLaser(int laserPin){
+int shoot_delay = 1000 / TX_SPEED; // (*)
+
+// Setup laser pin as output and receiver pin as input
+void setupLaserAndReceiver(int laserPin, int receiverPin){
   pinMode(laserPin, OUTPUT);
+  pinMode(receiverPin, INPUT);
 }
 
+// Just actuate the laser (for testing)
 void blinkLaser(int laserPin) {
   Serial.println("Laserzzz");
   analogWrite(laserPin, 50);
@@ -22,6 +28,7 @@ void blinkLaser(int laserPin) {
   delay(500);
 }
 
+// Shoot a bit value (*)
 void shootBit(int laserPin, int bit) {
   // int value = bit ? HI : LO;
   analogWrite(laserPin, bit ? HI : LO);
@@ -29,6 +36,7 @@ void shootBit(int laserPin, int bit) {
   analogWrite(laserPin, 0);
 }
 
+// Shoot some bits (*)
 void laserSequence(int laserPin) {
   //int seq = 0b1101u; //sequence;
   int n_bits = 4u;
@@ -40,3 +48,41 @@ void laserSequence(int laserPin) {
     shootBit(laserPin, bit);
   }
 }
+
+// Turn on the laser
+void turnLaserOn(int laserPin) {
+  digitalWrite(laserPin, HIGH);
+}
+
+// Turn off the laser
+void turnLaserOff(int laserPin) {
+  digitalWrite(laserPin, LOW);
+}
+
+// Check whether receiver is sensing the laser
+int isReceiverOn(int receiverPin) {
+  int value = analogRead(receiverPin);
+  Serial.println(value);
+  if (value <= RECEIVER_ON_LVL) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+// Check whether receiver is consistently sensing the laser
+int checkPaired(int receiverPin) {
+  if(isReceiverOn(receiverPin)) {
+    delay(1000); // Stop for 1s and check again
+    if(isReceiverOn(receiverPin)) {
+      // Lasers are paired.
+      return 1;
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
+}
+
+// (*): Deprecated funcionality
